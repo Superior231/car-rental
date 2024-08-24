f223320efb098413e34f507dda038c242dbcd537
Hikmal Falah <ikmalfalah@gmail.com>
2024-08-25 00:39:51 +0700

    Update {style}: add toast for error phone

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -59,6 +59,21 @@
     </div>
 @endif
 
+{{-- Phone --}}
+@if ($errors->has('phone'))
+    <div class="position-fixed p-3" id="toast-error" style="z-index: 99999;">
+        <div class="toast toast-error align-items-center" id="toast-error-content" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body d-flex align-items-center gap-2">
+                    <i class='bx bxs-x-circle text-danger fs-2'></i>
+                    <span class="ms-2 my-0 py-0 fw-medium">{{ $errors->first('phone') }}</span>
+                </div>
+                <button type="button" class="btn-close me-3 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+@endif
+
 {{-- Password --}}
 @if ($errors->has('password'))
     <div class="position-fixed p-3" id="toast-error" style="z-index: 99999;">
